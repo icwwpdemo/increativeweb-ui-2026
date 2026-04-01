963c8801101e5645bc61e34df59e19c7ef298882
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-04-01 19:52:47 +0530

    Update CSS

--- wp-content/themes/increativeweb/footer.php
+++ wp-content/themes/increativeweb/footer.php
@@ -16,7 +16,7 @@ $footer_bg = ( $footer_bg_image != '' ) ? 'data-background="' . esc_url( $footer
         <div class="container">
           <span class="sub-title">Hire Us To Change Your Brand</span>
           <h2 class="mw-1060">Do you have a project, maybe you are looking for creative solutions.</h2>
-          <a href="/contact-us/" class="icw-btn --primary">Let's Work Together<span class="arrow"></span></a>
+          <a href="/contact-us/" class="icw-btn --dark">Let's Work Together<span class="arrow"></span></a>
         </div>
     </div>   
     <div class="container">
